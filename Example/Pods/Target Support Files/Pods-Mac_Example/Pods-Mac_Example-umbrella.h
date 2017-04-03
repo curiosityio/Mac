4b6672096581d35ff222aa1709697d44d910ae96
Levi Bostian <levibostian@Levis-MacBook-Pro.local>
2017-04-03 15:49:34 -0500

    Initial commit. Created new cocoapods lib via CLI.

---
+++ Example/Pods/Target Support Files/Pods-Mac_Example/Pods-Mac_Example-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Mac_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Mac_ExampleVersionString[];
+
